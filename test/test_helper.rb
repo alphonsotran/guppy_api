45998be31166a36a726fd3411a4b4f03d68f2b64
Alphonso Tran <alphonsotran@gmail.com>
2020-05-09 01:33:46 +0200

    Purify db after each test

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,6 +3,10 @@ require_relative '../config/environment'
 require 'rails/test_help'
 
 class ActiveSupport::TestCase
+  parallelize_teardown do |worker|
+    Mongoid::Config.purge!
+  end
+
   # Run tests in parallel with specified workers
   parallelize(workers: :number_of_processors)
 
