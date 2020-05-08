851445bbdae8e753da29084a34425c1176fe9552
Alphonso Tran <alphonsotran@gmail.com>
2020-05-09 01:33:22 +0200

    Include encoder mixin into Url model

--- app/models/url.rb
+++ app/models/url.rb
@@ -1,5 +1,7 @@
 class Url
   include Mongoid::Document
+  include Encoder
+
   field :original_url, type: String
 
   validates :original_url, presence: { message: "Please type in a url" }
@@ -8,7 +10,7 @@ class Url
   before_validation :generate_shortener_as_id
 
   private
-  def generate_shortener_as_id
-    self._id = SecureRandom.uuid[0..6]
+  def generate_shortener_as_id(salt = false)
+    self._id = generate_hash_from_url(self.original_url, salt)
   end
 end
