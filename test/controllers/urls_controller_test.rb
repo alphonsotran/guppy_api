68224037cc401d266e20c5b25650032962bc4322
Alphonso Tran <alphonsotran@gmail.com>
2020-05-09 16:33:48 +0200

    Fix post path in tests

--- test/controllers/urls_controller_test.rb
+++ test/controllers/urls_controller_test.rb
@@ -9,14 +9,14 @@ class UrlsControllerTest < ActionDispatch::IntegrationTest
 
   test 'should create new url object' do
     assert_difference('Url.count') do
-      post '/links', params: { original_url: @link }
+      post links_path params: { original_url: @link }, headers: { "CONTENT_TYPE" => "text/javascript"}
     end
 
     assert_response :success
   end
 
   test 'should return status code 422 when url object is invalid' do
-    post '/links', params: { original_url: nil }
+    post links_path params: { original_url: nil }, headers: { "CONTENT_TYPE" => "text/javascript"}
 
     assert_response :unprocessable_entity
   end
