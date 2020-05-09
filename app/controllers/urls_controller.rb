fceb17b577c520d6e8db9aedaca5cdd1347eac22
Alphonso Tran <alphonsotran@gmail.com>
2020-05-09 19:36:56 +0200

    Add show action in url controller

--- app/controllers/urls_controller.rb
+++ app/controllers/urls_controller.rb
@@ -10,8 +10,17 @@ class UrlsController < ApplicationController
     end
   end
 
+  def show
+    begin
+      new_url = Url.find(url_params[:id])
+      redirect_to new_url.original_url
+    rescue Mongoid::Errors::DocumentNotFound => e
+      render 'errors/404', status: 404
+    end
+  end
+
   private
   def url_params
-    params.permit(:original_url)
+    params.permit(:original_url, :id)
   end
 end
