13325d9a24f140a81e915e92c31ddd22c4efa2d1
Alphonso Tran <alphonsotran@gmail.com>
2020-05-10 12:25:05 +0200

    Rename generate id method

--- app/models/url.rb
+++ app/models/url.rb
@@ -8,15 +8,14 @@ class Url
   validates :original_url, presence: { message: "Please type in a url" }
   validates :original_url, format: URI::regexp(%w[http https])
 
-  before_validation :generate_shortener_as_id
+  before_validation :generate_short_id
 
   def guppy_url
     # FIXME: Add env
     "http://localhost:3000/#{self._id}"
   end
 
-  private
-  def generate_shortener_as_id(salt = false)
+  def generate_short_id(salt = false)
     self._id = generate_hash_from_url(self.original_url, salt)
   end
 end
